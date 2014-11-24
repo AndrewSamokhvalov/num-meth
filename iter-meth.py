3da5be41e91e9958299b6b3db2f91cfa5f2f6233
AndrewSamokhvalov <andrew.shvv@gmail.com>
2014-11-24 13:10:34 +0300

    Add plot output

--- iter-meth.py
+++ iter-meth.py
@@ -3,20 +3,26 @@ __author__ = 'andrey'
 import numpy as np
 import time
 
-# from math import *
-# from matplotlib import pyplot as plt
-
-file_A = 'A.txt'
-file_b = 'b.txt'
-delimiter = ','
-mA = np.loadtxt(file_A, delimiter=delimiter)
-b = np.loadtxt(file_b, delimiter=delimiter)
+from math import *
+from matplotlib import pyplot as plt
 
 class Info():
     iteration = 0
     delay = 0
     eps = 0
     answer = 0
+    difference = 0
+
+    def __add__(self, other):
+        info = Info()
+        info.iteration = (self.iteration + other.iteration) / 2
+        info.delay = (self.delay, other.delay) / 2
+        info.iteration = (self.iteration + other.iteration) / 2
+        info.eps = (self.eps + other.eps) / 2
+        info.difference = (self.difference + other.difference) / 2
+        return info
+
+
 
     def description(self):
         print("iter count: %s" % self.iteration)
@@ -66,6 +72,7 @@ class QR(SolvingMethod):
 
     def solve(self, A, b):
         info = Info()
+        start = time.time()
 
         [n, n] = A.shape
 
@@ -79,10 +86,8 @@ class QR(SolvingMethod):
         R = - R
 
         info.answer = np.dot(np.dot(np.linalg.inv(R), Q,), b)
+        info.delay = time.time() - start
         return info
-
-
-
 
 
 class IIM(SolvingMethod):
@@ -96,7 +101,7 @@ class IIM(SolvingMethod):
 
     def solve(self, mA, b):
         info = Info()
-        info.eps = 0.1
+        info.eps = pow(10, -3)
 
         start = time.time()
         if not self.isSymetric(mA):
@@ -105,10 +110,8 @@ class IIM(SolvingMethod):
 
         xk = b.copy()
 
-
-        prev_xk = xk + 10 * info.eps
-
-        while info.eps < np.linalg.norm(prev_xk - xk):
+        isContinue = True
+        while isContinue:
             rk = self.get_r(xk, mA, b)
             tk_1 = self.get_t(rk, mA)
 
@@ -116,33 +119,63 @@ class IIM(SolvingMethod):
             xk = xk - tk_1 * rk
             info.iteration += 1
 
+            isContinue = info.eps < np.linalg.norm(prev_xk - xk)
+
         info.answer = xk
         info.delay = time.time() - start
 
         return info
 
 
+N = 500
+c = 3
+
 iim = IIM()
 qr = QR()
 
-for n in range(3, 20):
+l_info_qr = []
+l_info_iim = []
 
+def get_info(f, infos):
+    return list(map(f, infos))
+
+for n in range(3, N):
     mA = np.random.rand(n, n)
     b = np.random.rand(n)
 
-    answer = np.dot(np.linalg.inv(mA), b)
+    real_answer = np.dot(np.linalg.inv(mA), b)
 
     info_qr = qr.solve(mA, b)
     info_iim = iim.solve(mA, b)
-    
-    print("Answer")
-    print(answer)
 
-    print("QR:")
-    print(info_qr.answer)
+    info_qr.difference = np.linalg.norm(info_qr.answer - real_answer)
+    info_iim.difference = np.linalg.norm(info_iim.answer - real_answer)
 
-    print("IIM:")
-    print(info_iim.answer)
+    l_info_qr.append(info_qr)
+    l_info_iim.append(info_iim)
+
+    print("Progress: %.1f" % (n / N * 100), end="\r")
+
+
+l_qr_delays = [info.delay for info in l_info_qr]
+l_iim_delays = [info.delay for info in l_info_iim]
+
+l_qr_err = [info.difference for info in l_info_qr]
+l_iim_err = [info.difference for info in l_info_iim]
+
+plt.title("QR IIM comparison")
+plt.xlabel("N")
+plt.ylabel("T(n)")
+
+print("QR average error: %f" % (sum(l_qr_err) / len(l_qr_err)))
+print("IIM average error: %f " % (sum(l_iim_err) / len(l_iim_err)))
+
+
+plt.plot(l_qr_delays, 'o', label='qr delay')
+plt.plot(l_iim_delays, 's', label='iim delay')
+
+plt.show()
+
 
 
 
